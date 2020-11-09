5b0bec7922ae35c412003d59da895156fbbe77f0
Nicolás Acosta <nadesarrolloweb@gmail.com>
2020-11-09 04:08:37 -0300

    add support for get/add baggages. optimize queries

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -8,7 +8,13 @@ const getUsers = async (req, res) => {
     },
     order: [
       ['id', 'DESC']
-    ]
+    ],
+    attributes: ['id', 'name', 'flight'],
+    include: [{
+      model: db.Baggage,
+      as: 'baggages',
+      attributes: ['id', 'type']
+    }]
   });
   res.send({
     data: users
@@ -18,7 +24,13 @@ const getUsers = async (req, res) => {
 const addUser = async (req, res) => {
   const schema = Joi.object().keys({
     name: Joi.string(),
-    flight: Joi.string().max(5).required()
+    flight: Joi.string().max(5).required(),
+    baggages: Joi
+        .array()
+        .min(1)
+        .max(3)
+        .items(Joi.object().keys({type: Joi.number().required()}))
+        .required()
   });
 
   try {
@@ -30,8 +42,28 @@ const addUser = async (req, res) => {
     });
   }
 
-  const payload = {...req.body};
-  const user = await db.User.create(payload);
+  const {name, flight, baggages} = req.body;
+
+  // create user
+  const payload = {name, flight};
+  const {id: userId} = await db.User.create(payload);
+
+  // create baggages
+  await Promise.all(baggages.map(async (baggage) => {
+    await db.Baggage.create({...baggage, userId});
+  }));
+
+  const user = await db.User.findOne({
+    where: {
+      id: userId
+    },
+    attributes: ['id', 'name', 'flight'],
+    include: [{
+      model: db.Baggage,
+      as: 'baggages',
+      attributes: ['id', 'type']
+    }]
+  });
   res.send({
     data: user
   });
@@ -40,10 +72,9 @@ const addUser = async (req, res) => {
 const removeUser = async (req, res) => {
   const {id} = req.params;
   const payload = {status: 0};
-  const user = await db.User.update(payload, {where: {id}});
-  res.send({
-    data: user
-  });
+  await db.User.update(payload, {where: {id}});
+  await db.Baggage.update(payload, {where: {userId: id}});
+  res.send();
 };
 
 module.exports = {
